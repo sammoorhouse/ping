6ecabb09fc55ab8b6204d100b4dc1adc54cab61f
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-03-16 18:45:40 +0800

    form, post handler

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next){
+	//save results
+	
+	//redirect to results page
+	res.redirect('results/123456');
+});
+
+router.get(/^\/results\/(\w+)$/, function(req,res,next){
+	var id = req.params[0];
+	res.send(id);
+});
+
 module.exports = router;
